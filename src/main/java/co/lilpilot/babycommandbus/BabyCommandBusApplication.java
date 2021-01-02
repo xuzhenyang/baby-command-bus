4be5d57b5d54eae86e808f3e3536c48494fe894f
lilpilot <master@lilpilot.co>
2021-01-02 14:24:56 +0800

    init project

---
+++ src/main/java/co/lilpilot/babycommandbus/BabyCommandBusApplication.java
@@ -0,0 +1,13 @@
+package co.lilpilot.babycommandbus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabyCommandBusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BabyCommandBusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/lilpilot/babycommandbus/BabyCommandBusApplicationTests.java
@@ -0,0 +1,13 @@
+package co.lilpilot.babycommandbus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabyCommandBusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
